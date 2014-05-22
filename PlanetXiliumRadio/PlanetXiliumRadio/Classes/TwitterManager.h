3f535e64ca304a9ef006372c20a5bfd834e65fc1
juan felippo <juan@simpleapp.com.ar>
2014-05-22 14:15:47 -0300

    bug fixed

---
+++ PlanetXiliumRadio/PlanetXiliumRadio/Classes/TwitterManager.h
@@ -0,0 +1,13 @@
+//
+//  TwitterManager.h
+//  PlanetXiliumRadio
+//
+//  Created by juan felippo on 20/05/14.
+//  Copyright (c) 2014 Ramiro Ponce. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TwitterManager : NSObject
+
+@end
---
+++ PlanetXiliumRadio/PlanetXiliumRadio/Classes/TwitterManager.m
@@ -0,0 +1,13 @@
+//
+//  TwitterManager.m
+//  PlanetXiliumRadio
+//
+//  Created by juan felippo on 20/05/14.
+//  Copyright (c) 2014 Ramiro Ponce. All rights reserved.
+//
+
+#import "TwitterManager.h"
+
+@implementation TwitterManager
+
+@end
